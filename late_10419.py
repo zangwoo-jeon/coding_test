0db9aaad62e4a2eddded33d2452bc8bd5ead78c1
zangwoo Jeon <61596931+zangwoo-jeon@users.noreply.github.com>
2022-11-15 22:46:03 +0900

    Update late_10419.py

--- late_10419.py
+++ late_10419.py
@@ -2,6 +2,12 @@ T = int(input())
 
 for _ in range(T):
     d = int(input())
+    t = 0
+    while (t+1) + (t+1)**2 <= d:
+        t += 1
+    print(t)
+    
+    '''
     count = 0
     for i in range(1, 101):
         if d >= i + i**2:
@@ -9,3 +15,5 @@ for _ in range(T):
         else:
             print(count)
             break
+    '''
+    
