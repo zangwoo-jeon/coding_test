f15cbd1d58fcaff93f7775fc3dbbaf6d1d5cd023
zangwoo Jeon <61596931+zangwoo-jeon@users.noreply.github.com>
2022-11-19 01:47:20 +0900

    Update island_number_4963.py

--- island_number_4963.py
+++ island_number_4963.py
@@ -35,3 +35,35 @@ while True:
 
 for i in answer:
     print(i)
+    
+'''
+from collections import deque
+
+dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+dy = [0, 0, -1, 1, -1, 1, -1, 1]
+
+def bfs(x, y):
+    Q = deque()
+    Q.append((x, y))
+    while Q:
+        x, y = Q.popleft()
+        for i in range(len(dx)):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                Q.append((nx, ny))
+    return True
+
+while True:
+    w, h = map(int, input().split())
+    if w == 0 and h == 0:
+        break
+    graph = [list(map(int, input().split())) for _ in range(h)]
+    result = 0
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1 and bfs(i, j) == True:
+                result += 1
+    print(result)
+'''
