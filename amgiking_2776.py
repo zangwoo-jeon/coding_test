ef795bcad0f1cbff7b745f93dfba2fc83ba7e1b8
zangwoo Jeon <61596931+zangwoo-jeon@users.noreply.github.com>
2022-11-20 02:39:01 +0900

    Create amgiking_2776.py

---
+++ amgiking_2776.py
@@ -0,0 +1,22 @@
+T = int(input())
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return 0
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return 1
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    else:
+        return binary_search(array, target, mid+1, end)
+
+for _ in range(T):
+    n = int(input())
+    array = list(map(int, input().split()))
+    array.sort()
+    m = int(input())
+    find = list(map(int, input().split()))
+
+    for i in find:
+        print(binary_search(array, i, 0, n-1))
